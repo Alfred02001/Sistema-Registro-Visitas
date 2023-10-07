763c67ddf3ed8fbda6bdeb6eb40b552fdb37b57a
Alfredo Fermin <93239848+Alfred02001@users.noreply.github.com>
2023-10-06 23:35:36 -0400

    Add files via upload

---
+++ Capa_Datos/D_Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Capa_Datos
+{
+    public class D_Conexion
+    {
+        private readonly string cnxx;
+
+        protected SqlConnection cxn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+
+        public D_Conexion()
+        {
+            cnxx = (ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+
+        }
+        protected SqlConnection GetCnx()
+        {
+            return new SqlConnection(cnxx);
+        }
+    }
+}
